a29172be7a6e3212016ff8491da60d7535f3a8bb
Venkat AVINASH Karra <35541558+aviboot92@users.noreply.github.com>
2021-12-01 17:13:19 +0530

    Pages, Routing and Head system (#2)
    * Basic meta Tag is added for Routing
    
    * Sample Routing example is added
    
    * About Page is added successfully
    
    Co-authored-by: avinash.k.v <avinash.k@techsophy.com>

---
+++ pages/about.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const defaultProps = {};
+
+/**@uthor- VAK
+ *
+ */
+const About = () => {
+  return (
+    <>
+      <h1>About</h1>
+    </>
+  );
+};
+
+// #endregion
+
+export default About;
