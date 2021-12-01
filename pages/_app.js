c7ff40515111775b79fc19adf90b8610a1ce98d4
Venkat AVINASH Karra <35541558+aviboot92@users.noreply.github.com>
2021-12-01 23:41:32 +0530

    Custom Meta Tag is added (#11)
    Co-authored-by: avinash.k.v <avinash.k@techsophy.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
-import Nav from "./../components/Nav";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Nav />
       <Layout>
         <Component {...pageProps} />
       </Layout>
