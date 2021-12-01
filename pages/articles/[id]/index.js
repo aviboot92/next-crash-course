76c6011e123b19edaa52cd6d23655d50ddc0de2b
Venkat AVINASH Karra <35541558+aviboot92@users.noreply.github.com>
2021-12-01 20:41:20 +0530

    getServerSideProps (#7)
    Co-authored-by: avinash.k.v <avinash.k@techsophy.com>

--- pages/articles/[id]/index.js
+++ pages/articles/[id]/index.js
@@ -1,12 +1,35 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
+import Link from "next/link";
 
 /**
  *
  */
-const Article = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  return <div>This is an Article with an id {id}</div>;
+const Article = ({ article }) => {
+  // const router = useRouter();
+  // const { id } = router.query;
+  return (
+    <div>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <button>
+        <Link href="/">Go Back</Link>
+      </button>
+    </div>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
 };
 
 // #endregion
